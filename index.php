d3204bed080f6cb40eed5b00510826e9f091c847
Jean <mngs.jean@gmail.com>
2019-01-14 00:09:02 +0100

    Mise en place de la localisation de place en fonction d'un nombre de kilomètre autour de soit.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<title>Random destination</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="main.css">
-	<script src="https://maps.google.com/maps/api/js?key=AIzaSyBxDAXNxWZ8D0Es3Bk9_f2DYzhItk2oDrA" type="text/javascript"></script>
+	<script src="https://maps.google.com/maps/api/js?key=AIzaSyBxDAXNxWZ8D0Es3Bk9_f2DYzhItk2oDrA&libraries=places" type="text/javascript"></script>
 	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 </head>
 <body>
@@ -12,10 +12,11 @@
 		<h1>Random destination</h1>
 	</header>
 	<main>
+		<div id="informationDiv"></div>
 		<div id="mapContainer">
 		</div>
 		<div id="formDistance">
-			<input type="number" id="distance" name="distance">
+			<p><input type="number" id="distance" name="distance"> <span>KM<span></p>
 			<button id="submit">Search</button>
 		</div>
 	</main>
--- main.js
+++ main.js
@@ -2,14 +2,38 @@
 
 var mapObject = function(){
 	var self = this;
+	self.apiKey = 'AIzaSyBxDAXNxWZ8D0Es3Bk9_f2DYzhItk2oDrA';
+	self.distance = 0;
+	self.lng;
+	self.lat;
+	self.myPos;
+	self.myPosImage;
+	self.map;
+	self.markerMyPos;
+	self.markerDestination;
+	self.dataRandom;
 
-	self.myPosImage = {
-	    url: 'http://alomea.fr/wp-content/uploads/2016/12/localisation-icon.png',
-	    size: new google.maps.Size(71, 71),
-	  	origin: new google.maps.Point(0, 0),
-	  	anchor: new google.maps.Point(17, 34),
-	  	scaledSize: new google.maps.Size(45, 45)
-  	};
+	self.earthRadii = {
+        mi: 3963.1676,
+        km: 6378.1,
+        ft: 20925524.9,
+        mt: 6378100,
+        in: 251106299,
+        yd: 6975174.98,
+        fa: 3487587.49,
+        na: 3443.89849,
+        ch: 317053.408,
+        rd: 1268213.63,
+        fr: 31705.3408
+    }
+
+	self.myPosImage = new google.maps.MarkerImage(
+	    'http://alomea.fr/wp-content/uploads/2016/12/localisation-icon.png',
+	    new google.maps.Size(45, 45),
+	  	new google.maps.Point(0, 0),
+	  	new google.maps.Point(17, 34),
+	  	new google.maps.Size(45, 45)
+  	);
 
 	self.start = function(){
 		self.getPosition();
@@ -30,7 +54,7 @@ var mapObject = function(){
 			// Nous plaçons le centre de la carte avec les coordonnées ci-dessus
 			center: new google.maps.LatLng(self.lat, self.lng), 
 			// Nous définissons le zoom par défaut
-			zoom: 15, 
+			zoom: 10, 
 			// Nous définissons le type de carte (ici carte routière)
 			// Nous activons les options de contrôle de la carte (plan, satellite...)
 			mapTypeControl: true,
@@ -54,24 +78,55 @@ var mapObject = function(){
 			icon: self.myPosImage,
 			map: self.map
 		})
-	}
-}
 
-var setDestination = function(){
-	var self = this;
-	self.distance = $('#distance').val();
+		$('#submit').click(function(){
+			self.initDestination();
+		})
+	}
 
 	self.initDestination = function(){
-		console.log(self.distance);
+		self.distance = $('#distance').val();
+		var request = {
+		    location: self.myPos,
+		    radius: (self.distance/self.earthRadii.km) * self.earthRadii.mt,
+		    type: ['night_club']
+
+		};
+
+		self.service = new google.maps.places.PlacesService(self.map);
+		self.service.nearbySearch(request, function(retour){
+			self.setRandomPoint(retour);  	
+		});
+	}
+
+	self.setRandomPoint = function(data){
+		console.log(data);
+		var dataLength = data.length;
+		var random = Math.ceil(Math.random()*dataLength-1);
+		console.log(random);
+		self.dataRandom = data[random];
+		self.setMarker();
+	}
+
+	self.setMarker = function(){
+		console.log(self.dataRandom);
+		var posi = self.dataRandom.geometry.location;
+		var distanceBetween = '';
+		$('#informationDiv').html('<p>Nom : '+self.dataRandom.name+'</p><p>'+distanceBetween+'</p><p>'+self.dataRandom.vicinity+'</p><p>Note : '+self.dataRandom.rating+'/5</p>');
+
+
+		if(typeof(self.markerDestination) !== 'undefined'){
+			self.markerDestination.setMap(null);
+		}
+		self.markerDestination = new google.maps.Marker({
+			position: posi,
+			map: self.map
+		})
 	}
 }
+
 
 $(document).ready(function(){
 	var mapTest = new mapObject();
 	mapTest.start();
-});
-
-$('#submit').click(function(){
-	var destination = new setDestination();
-	destination.initDestination();
 });
